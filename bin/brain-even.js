af2c99aa95d75c05d4866eba834b14f391c27be6
ressw <resswert@gmail.com>
2023-10-24 02:10:07 +0500

    Modify bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,42 @@
 #!/usr/bin/env node
 
-import getName from '../src/cli.js';
+import getAnswer from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const username = getName();
-console.log(`Hello, ${username}!`);
+const getRandomNumber = () => Math.floor(Math.random() * 9);
+const isEven = (num) => num % 2 === 0;
+
+const gameOver = (username, answer, correctAnswer) => {
+  console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${username}!`);
+};
+
+const ask = (username) => {
+  const number = getRandomNumber();
+  const check = isEven(number) ? 'yes' : 'no';
+  console.log(`Question: ${number}`);
+  const answer = getAnswer('Your answer: ');
+  if (check === answer) {
+    console.log('Correct!');
+    console.log();
+    return true;
+  }
+  gameOver(username, answer, check);
+
+  return false;
+};
+
+const run = () => {
+  console.log('Welcome to the Brain Games!');
+  const username = getAnswer('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log();
+  if (!ask(username)) return false;
+  if (!ask(username)) return false;
+  if (!ask(username)) return false;
+  console.log(`Congratulations, ${username}!`);
+
+  return true;
+};
+
+run();
