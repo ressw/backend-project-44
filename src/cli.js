eb3a792172345348df48e76655d91b5bb1bbc64d
ressw <resswert@gmail.com>
2023-10-24 02:07:56 +0500

    Modify src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,5 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => {
-  const question = 'May I have your name?';
-  return readlineSync.question(`${question}: `);
-};
+const getAnswer = (question) => readlineSync.question(`${question}`);
 
-export default getName;
+export default getAnswer;
