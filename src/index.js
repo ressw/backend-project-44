9e4ee893021eae716962a2d9388fb18e10e9bea5
ressw <resswert@gmail.com>
2023-10-31 02:48:07 +0500

    Create index.js in the src directory

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+
+export const getUserAnswer = (question) => readlineSync.question(`${question}`);
+
+export const questionsNumber = 3;
+
+export const getRandomNumber = (max = 9) => Math.floor(Math.random() * max);
+
+export const isEven = (num) => num % 2 === 0;
+
+export const gameOver = (username, answer, correctAnswer) => {
+  console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${username}!`);
+};
+
+export const start = () => {
+  console.log('Welcome to the Brain Games!');
+  const username = getUserAnswer('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+  console.log();
+  return username;
+};
