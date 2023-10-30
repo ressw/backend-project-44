2b63e37a957fcdf3fc0017b3e0df20b709cce17b
ressw <resswert@gmail.com>
2023-10-31 02:44:43 +0500

    Create a games directory in the bin directory

---
+++ bin/games/brain-even.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import {
+  start, questionsNumber, getRandomNumber,
+  isEven, getUserAnswer, gameOver,
+} from '../../src/index.js';
+
+const run = () => {
+  const username = start();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log();
+
+  let attemp = 0;
+  while (attemp < questionsNumber) {
+    const number = getRandomNumber();
+    const check = isEven(number) ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const answer = getUserAnswer('Your answer: ');
+
+    if (check === answer) {
+      console.log('Correct!');
+      console.log();
+      attemp += 1;
+    } else {
+      gameOver(username, answer, check);
+      return false;
+    }
+  }
+  console.log(`Congratulations, ${username}!`);
+  return true;
+};
+
+run();
