b4b316a5841e6b0ac704d2ec7625d3cb02742b94
ressw <resswert@gmail.com>
2023-10-31 18:41:17 +0500

    Make bin/games/brain-gcd.js

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import {
+  start, getUserAnswer,
+  getRandomNumber, gameOver, questionsNumber,
+} from '../../src/index.js';
+
+const getNumbersPair = () => {
+  const divisors = [2, 3, 5];
+  const leastDivisor = divisors[getRandomNumber(3)];
+  let randomMultiplier = getRandomNumber(20);
+  const firstNumber = leastDivisor * randomMultiplier;
+  randomMultiplier = getRandomNumber(20);
+  const secondNumber = leastDivisor * randomMultiplier;
+  return [firstNumber, secondNumber];
+};
+
+const getGreatestDivisor = (firstNumber, secondNumber) => {
+  let minNum = firstNumber;
+  let maxNum = secondNumber;
+  if (minNum > maxNum) {
+    minNum = secondNumber;
+    maxNum = firstNumber;
+  }
+
+  if (maxNum % minNum === 0) return minNum;
+  let divisor = Math.trunc(minNum / 2);
+  while (divisor > 1) {
+    if (maxNum % divisor === 0 && minNum % divisor === 0) {
+      return divisor;
+    }
+    divisor -= 1;
+  }
+  return divisor;
+};
+
+const run = () => {
+  const username = start();
+  console.log('Find the greatest common divisor of given numbers.');
+  console.log();
+
+  let attemp = 0;
+  while (attemp < questionsNumber) {
+    const [firstNumber, secondNumber] = getNumbersPair();
+    const greatestDivisor = getGreatestDivisor(firstNumber, secondNumber);
+    console.log(`Question: ${firstNumber} ${secondNumber}`);
+    const userAnswer = getUserAnswer('Your answer: ');
+
+    if (greatestDivisor === parseInt(userAnswer, 10)) {
+      console.log('Correct!');
+      console.log();
+      attemp += 1;
+    } else {
+      gameOver(username, userAnswer, greatestDivisor);
+      return false;
+    }
+  }
+
+  console.log(`Congratulations, ${username}!`);
+  return true;
+};
+
+run();
+
+// console.log(getGreatestDivisor(2, 9));
