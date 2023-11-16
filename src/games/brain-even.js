0df709aa46686240044f88c81b8698f35050b1a4
ressw <resswert@gmail.com>
2023-11-16 23:21:24 +0500

    Update src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-import { runGame, isEven, getRandomNumber } from '../index.js';
+import { runGame, getRandomNumber } from '../index.js';
+
+export const isEven = (num) => num % 2 === 0;
 
 const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
 
