7b0da5f61263232c458a110c7f2453169438e9f5
ressw <resswert@gmail.com>
2023-11-17 00:59:32 +0500

    Update src/games/brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -22,7 +22,7 @@ const gameQuestion = 'Answer "yes" if given number is prime. Otherwise answer "n
 const genNewQuestion = () => {
   const question = getRandomNumber(1, 350);
   const correctAnswer = isPrimeNumber(question) ? 'yes' : 'no';
-  return [question, correctAnswer];
+  return [String(question), correctAnswer];
 };
 
 export default () => {
